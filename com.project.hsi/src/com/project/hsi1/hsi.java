d7d0de58264d4e444f7f9e449a49a68fee6ef8d3
User <User@DESKTOP-9UFK3PD>
2022-12-05 12:28:20 +0530

    hsi push

---
+++ com.project.hsi/src/com/project/hsi1/hsi.java
@@ -0,0 +1,22 @@
+package com.project.hsi1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class hsi {
+	public static void main(String[] args) {
+		String URL = "jdbc:mysql://localhost:3306/student";
+		String UserName = "root";
+		String Password = "devoloper";
+		Connection connection = null;
+		try {
+			connection=DriverManager.getConnection(URL, UserName, Password);
+			System.out.println("Connection is sucessfully establised");
+		}
+		catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
